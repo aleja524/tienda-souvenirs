eca274bd825dc7ad104af2c26db8c440093ef51f
Alejandra <aleja.escobar2501@gmail.com>
2025-08-16 16:29:05 -0500

    clase producto

---
+++ tienda.py
@@ -0,0 +1,6 @@
+class Producto:
+    def __init__(self, codigo, nombre, categoria, precio):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
