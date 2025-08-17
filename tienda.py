74e26651b4f877ce91baa4436aad4e38f764a6ee
Alejandra <aleja.escobar2501@gmail.com>
2025-08-16 20:17:12 -0500

    str y busqueda binaria

--- tienda.py
+++ tienda.py
@@ -4,3 +4,9 @@ class Producto:
         self.nombre = nombre
         self.categoria = categoria
         self.precio = precio
+    
+    def __str__(self):
+        return f"[{self.codigo}] {self.nombre} - {self.categoria} - {self.precio}"
+    
+    def busqueda_binaria(productos, nombre, inicio= 0, fin= None):
+        pass
