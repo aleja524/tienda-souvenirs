f9e05d0397d4ffdc65ddb07a695388da1a076736
Alejandra <aleja.escobar2501@gmail.com>
2025-08-16 20:32:30 -0500

    funciones recursivas

--- tienda.py
+++ tienda.py
@@ -10,3 +10,18 @@ class Producto:
     
     def busqueda_binaria(productos, nombre, inicio= 0, fin= None):
         pass
+
+    def precio_total(productos, i=0, acumulado=0):
+        pass
+
+    def promedio_categoria(productos, categoria, i=0, suma=0, cantidad=0):
+        pass
+
+    def ordenamiento_precio(productos, ascendente=True):
+        pass
+
+    def productos_en_rango(productos, minimo, maximo, i=0, resultado=None):
+        pass
+
+    def recomendaciones(productos, producto_base, i=0, resultado=None):
+        pass
