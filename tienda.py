520e8ef17771d432f79b20da160dc4f292047fe3
Alejandra <aleja.escobar2501@gmail.com>
2025-08-16 21:05:03 -0500

    funciones recursivas corregidas y lista nueva

--- tienda.py
+++ tienda.py
@@ -5,23 +5,40 @@ class Producto:
         self.categoria = categoria
         self.precio = precio
     
-    def __str__(self):
-        return f"[{self.codigo}] {self.nombre} - {self.categoria} - {self.precio}"
+def __str__(self):
+    return f"[{self.codigo}] {self.nombre} - {self.categoria} - {self.precio}"
     
-    def busqueda_binaria(productos, nombre, inicio= 0, fin= None):
-        pass
+def busqueda_binaria(productos, nombre, inicio= 0, fin= None):
+    pass
 
-    def precio_total(productos, i=0, acumulado=0):
-        pass
+def precio_total(productos, i=0, acumulado=0):
+    pass
 
-    def promedio_categoria(productos, categoria, i=0, suma=0, cantidad=0):
-        pass
+def promedio_categoria(productos, categoria, i=0, suma=0, cantidad=0):
+    pass
 
-    def ordenamiento_precio(productos, ascendente=True):
-        pass
+def ordenamiento_precio(productos, ascendente=True):
+    pass
 
-    def productos_en_rango(productos, minimo, maximo, i=0, resultado=None):
-        pass
+def productos_en_rango(productos, minimo, maximo, i=0, resultado=None):
+    pass
 
-    def recomendaciones(productos, producto_base, i=0, resultado=None):
-        pass
+def recomendaciones(productos, producto_base, i=0, resultado=None):
+    pass
+
+def imprimir_productos(productos, i=0):
+    if i == len(productos):
+        return
+    print(productos[i])
+    imprimir_productos(productos, i+1)
+
+
+
+productos = [
+    Producto("001", "Artesania maya", "artesanias", "artesanias", 15000),
+    Producto("002", "Camisa bordada", "ropa", 40000),
+    Producto("003", "iman medellin", "imanes", 8000),
+    Producto("004", "bufanda andina", "ropa", 25000)
+]
+
+
